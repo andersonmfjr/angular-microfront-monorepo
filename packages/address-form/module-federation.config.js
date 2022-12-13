304d0241f2f7793ea0e0c490e436576e0619748c
Anderson Feitosa <andersonmfjr@gmail.com>
2022-12-13 19:36:39 -0300

    feat: add shell and address form mfe

---
+++ packages/address-form/module-federation.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'address-form',
+  exposes: {
+    './Module': 'packages/address-form/src/app/remote-entry/entry.module.ts',
+  },
+};
---
+++ packages/address-form/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'address-form-entry',
+  template: `<nx-welcome></nx-welcome>`,
+})
+export class RemoteEntryComponent {}
